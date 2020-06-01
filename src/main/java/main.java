0a7a464ffcaab53fa5dec99d8f03bf23e604e96d
akbarsaputrait <akbarsaputrait@outlook.com>
2020-06-01 23:05:30 +0700

    Check internet if exist or not

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,17 +1,45 @@
 import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        new LoginStage(primaryStage);
+        if (netIsAvailable()) {
+            new LoginStage(primaryStage);
+        } else {
+            Alert alertAdd = new Alert(Alert.AlertType.ERROR,
+                    "Pastikan anda terhubung dengan koneksi internet. Silahkan coba lagi", ButtonType.CLOSE);
+            alertAdd.showAndWait();
+            if (alertAdd.getResult() == ButtonType.CLOSE) {
+                System.exit(1);
+            }
+        }
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private static boolean netIsAvailable() {
+        try {
+            final URL url = new URL("http://www.google.com");
+            final URLConnection conn = url.openConnection();
+            conn.connect();
+            conn.getInputStream().close();
+            return true;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
 
