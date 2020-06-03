ec830859f68200f5b6e27b679ce2709d20eb9e36
akbarsaputrait <akbarsaputrait@outlook.com>
2020-06-03 20:45:27 +0700

    Fix some bug

--- src/main/java/Loading.java
+++ src/main/java/Loading.java
@@ -11,35 +11,49 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-class Loading {
-    Scene getScene(){
+import java.io.File;
+import java.util.Objects;
+
+public class Loading {
+    private int timeSeconds;
+    private Timeline timer;
+
+    Scene getScene() {
         GridPane loadGrid = new GridPane();
         loadGrid.setAlignment(Pos.CENTER);
         loadGrid.setPadding(new Insets(50, 50, 50, 50));
         ImageView imageview = new ImageView();
-        try{
-            Image i = new Image(this.getClass().getResourceAsStream("load.gif"));
+
+        try {
+            Image i = new Image(new File(Objects.requireNonNull(this.getClass().getClassLoader().getResource(
+                    "load.gif"
+            ).getPath())).toURI().toString());
             imageview.setImage(i);
-        }catch (Exception ignored){}
-        loadGrid.add(imageview ,1,1);
+        } catch (Exception ignored) {
+        }
+
+        loadGrid.add(imageview, 1, 1);
+
         return new Scene(loadGrid, 300, 275);
     }
-    private int timeSeconds;
-    private Timeline timer;
-    Scene getDaftarNoticeScene(Stage stage, Scene scene, String pesan){
+
+
+    Scene getDaftarNoticeScene(Stage stage, Scene scene, String pesan) {
         GridPane noticeGrid = new GridPane();
         noticeGrid.setAlignment(Pos.CENTER);
         noticeGrid.setPadding(new Insets(50, 50, 50, 50));
         Label notice = new Label(pesan);
         notice.setFont(Font.font(17));
-        noticeGrid.add(notice ,1,1);
+        noticeGrid.add(notice, 1, 1);
         timeSeconds = 2;
         timer = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
             timeSeconds--;
             if (timeSeconds <= 0) {
                 stage.setScene(scene);
                 timer.stop();
+
             }
+
         }));
         timer.setCycleCount(5);
         timer.playFromStart();
